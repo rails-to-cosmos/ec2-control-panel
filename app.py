6a0fefe46627d4cfd52ad6199da6c84b45027d65
Dmitry Akatov <dmitry.akatov@protonmail.com>
2025-02-07 15:14:10 +0100

    minors

--- app.py
+++ app.py
@@ -135,8 +135,8 @@ def __(ClientError, boto3, functools):
 
 @app.cell
 def __(AVAILABILITY_ZONE, boto3, mo):
-    ec2 = boto3.client('ec2')
-    instance_types = ["g6e.xlarge"]
+    ec2 = boto3.client("ec2")
+    instance_types = []
 
     paginator = ec2.get_paginator("describe_instance_type_offerings")
     for page in paginator.paginate(LocationType="availability-zone", Filters=[{"Name": "location", "Values": [AVAILABILITY_ZONE]}]):
